981f1f0e6e83eceb584a8c3c34f4fd6118d0cdd7
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-08-21 23:49:54 +0200

    gui fixes

--- control_cluster_utils/utilities/plot_utils.py
+++ control_cluster_utils/utilities/plot_utils.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QSplitter, QLabel, QGridLayout
 
 import pyqtgraph as pg
 
-from typing import List
+from typing import List, Callable
 
 class RealTimePlotApp(QMainWindow):
 
@@ -33,7 +33,9 @@ class RealTimePlotApp(QMainWindow):
                                 main_window=self)
         
         self.central_widget.setLayout(self.layout)
-        
+
+        self.show()
+   
 class RtPlotWidget(pg.PlotWidget):
 
     def __init__(self, 
@@ -41,17 +43,18 @@ class RtPlotWidget(pg.PlotWidget):
                 n_data: int,  
                 update_dt: float, 
                 base_name: str, 
-                plot_frame: QFrame):
+                parent: QWidget = None):
 
-        super().__init__(title=base_name)
-
+        super().__init__(title=base_name,
+                    parent=parent)
+        
         self.timestamps_red_factor = 10
-
-        self.plot_frame = plot_frame  # Store the reference to the plot frame
         
         self.n_data = n_data
 
-        self.window_size = int(window_duration // update_dt) # floor division
+        self.window_offset = 0
+
+        self.window_size = int(window_duration // update_dt) + 1 # floor division
         self.window_duration = window_duration
 
         self.window_buffer_factor = 2
@@ -154,9 +157,9 @@ class RtPlotWidget(pg.PlotWidget):
 
             self._update_timestams_ticks(elapsed_times) 
 
-            x_range = (self.window_buffer_size - 1 - self.window_size, 
-                self.window_buffer_size - 1) 
-            self.setXRange(*x_range)
+            # x_range = (self.window_buffer_size - 1 - self.window_size, 
+            #     self.window_buffer_size - 1) 
+            # self.setXRange(*x_range)
 
             self.last_update_time = current_time
 
@@ -183,106 +186,140 @@ class RtPlotWidget(pg.PlotWidget):
         
         self.window_size = min(new_size, self.window_buffer_size)
 
-        x_range = (self.window_buffer_size - 1 - self.window_size, 
-            self.window_buffer_size - 1) 
+        x_range = (self.window_buffer_size - 1 - self.window_size - self.window_offset * self.window_size, 
+            self.window_buffer_size - 1 - self.window_offset * self.window_size) 
         
         self.setXRange(*x_range)
+
+    def update_window_offset(self, 
+                    offset: int = 0):
         
-class DataThread(QThread):
+        current_window_buffer_factor = int(self.window_buffer_duration // self.window_size)
 
-    data_updated = pyqtSignal(np.ndarray)
+        if offset >= current_window_buffer_factor:
 
-    def __init__(self, 
-                rt_plotter: RtPlotWidget, 
-                n_data: int):
+            offset = current_window_buffer_factor - 1
+
+        self.window_offset = offset
         
-        super().__init__()
-
-        self.rt_plot_widget = rt_plotter
-
-        self.n_data = n_data
-
-    def run(self):
-
-        while True:
-
-            new_data = 2 * (np.random.rand(self.n_data, 1) - np.full((self.n_data, 1), 0.5))
-
-            self.data_updated.emit(new_data)
-
-            time.sleep(self.rt_plot_widget.update_dt)
-
-class SettingsWidget(QWidget):
+class SettingsWidget():
 
     def __init__(self, 
             rt_plotter: RtPlotWidget,
-            parent_frame: QFrame = None
+            parent: QWidget = None
             ):
-        
-        super().__init__(parent_frame)
 
         self.rt_plot_widget = rt_plotter
 
-        self.parent_frame = parent_frame
+        self.frame = QFrame(parent=parent)
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.settings_frame_layout = QVBoxLayout(self.frame)  # Use QVBoxLayout here
+        self.settings_frame_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.val_frames = []
+        self.val_layouts = []
+        self.current_vals = []
+        self.val_slider_frames = []
+        self.val_slider_frame_layouts = []
+        self.min_labels = []
+        self.max_labels = []
+        self.val_sliders = []
 
         self.init_ui()
 
+    def _generate_complex_slider(self, 
+                        parent: QWidget, 
+                        callback: Callable[[int], None], 
+                        min_shown: str, 
+                        min: int,
+                        max_shown: str, 
+                        max: int,
+                        init_val_shown: str,
+                        init: int,
+                        title: str):
+
+        val_frame = QFrame(parent)
+        val_frame.setFrameShape(QFrame.StyledPanel)
+        val_layout = QHBoxLayout(val_frame)  # Use QVBoxLayout here
+        val_layout.setContentsMargins(2, 2, 2, 2)
+
+        self.val_title = QLabel(title)
+        current_val = QLabel(init_val_shown)
+        current_val.setAlignment(Qt.AlignRight)
+        current_val.setStyleSheet("border: 1px solid gray; background-color: white; border-radius: 4px;")
+
+        val_layout.addWidget(self.val_title, 
+                                    alignment=Qt.AlignLeft)
+        val_layout.addWidget(current_val)
+
+        val_slider_frame = QFrame(parent)
+        val_slider_frame.setFrameShape(QFrame.StyledPanel)
+        val_slider_frame_layout = QHBoxLayout(val_slider_frame)  # Use QHBoxLayout here
+        val_slider_frame_layout.setContentsMargins(2, 2, 2, 2)
+
+        min_label = QLabel(min_shown)
+        min_label.setAlignment(Qt.AlignCenter)
+        min_label.setStyleSheet("border: 1px solid gray; background-color: white; border-radius: 4px;")
+        val_slider_frame_layout.addWidget(min_label)
+
+        val_slider = QSlider(Qt.Horizontal)
+        val_slider.setMinimum(min)
+        val_slider.setMaximum(max)
+        val_slider.setValue(init)
+        val_slider.valueChanged.connect(callback)
+        val_slider_frame_layout.addWidget(val_slider)
+
+        max_label = QLabel(max_shown)
+        max_label.setAlignment(Qt.AlignCenter)
+        max_label.setStyleSheet("border: 1px solid gray; background-color: white; border-radius: 4px;")
+        val_slider_frame_layout.addWidget(max_label)
+
+        self.settings_frame_layout.addWidget(val_frame)
+        self.settings_frame_layout.addWidget(val_slider_frame)
+
+        self.val_frames.append(val_frame)
+        self.val_layouts.append(val_layout)
+        self.val_slider_frames.append(val_slider_frame)
+        self.val_slider_frame_layouts.append(val_slider_frame_layout)
+        self.min_labels.append(min_label)
+        self.max_labels.append(max_label)
+        self.val_sliders.append(val_slider)
+        self.current_vals.append(current_val)
+
     def init_ui(self):
-        
-        self.settings_layout = QVBoxLayout(self.parent_frame)
-        self.settings_layout.setContentsMargins(5, 5, 5, 5)
-
-        # create a scroll area for the legend names
-        self.settings_scroll_area = QScrollArea(self.parent_frame)
-        self.settings_scroll_area.setWidgetResizable(True)
-        self.settings_scroll_content = QWidget(self.settings_scroll_area)
-        self.settings_scroll_layout = QVBoxLayout(self.settings_scroll_content)
-        
+ 
         # create slider for window size
-        self.window_size_frame = QFrame(self.settings_scroll_area)
-        self.window_size_frame.setFrameShape(QFrame.StyledPanel)
-        self.window_size_layout = QVBoxLayout(self.window_size_frame)  # Use QVBoxLayout here
-        self.window_size_layout.setContentsMargins(5, 5, 5, 5)
 
-        self.window_size_title = QLabel("window size [s]")
-        self.window_size_layout.addWidget(self.window_size_title, alignment=Qt.AlignHCenter)
+        self._generate_complex_slider(title="window size [s]: ", 
+                                parent=self.frame, 
+                                callback=self.update_window_size, 
+                                min_shown=f'{self.rt_plot_widget.update_dt}', 
+                                min = 1,
+                                max_shown=f'{self.rt_plot_widget.window_buffer_duration}', 
+                                max = self.rt_plot_widget.window_buffer_size,
+                                init_val_shown =f'{self.rt_plot_widget.update_dt * self.rt_plot_widget.window_size}', 
+                                init=self.rt_plot_widget.window_size)
 
-        self.window_size_slider_frame = QFrame(self.settings_scroll_area)
-        self.window_size_slider_frame.setFrameShape(QFrame.StyledPanel)
-        self.window_size_slider_frame_layout = QHBoxLayout(self.window_size_slider_frame)  # Use QHBoxLayout here
-        self.window_size_slider_frame_layout.setContentsMargins(5, 5, 5, 5)
-
-        self.min_label = QLabel(f'{self.rt_plot_widget.update_dt}')
-        self.min_label.setAlignment(Qt.AlignCenter)
-        self.min_label.setStyleSheet("border: 1px solid gray; background-color: white; border-radius: 4px;")
-        self.window_size_slider_frame_layout.addWidget(self.min_label)
-
-        self.window_size_slider = QSlider(Qt.Horizontal)
-        self.window_size_slider.setMinimum(1)
-        self.window_size_slider.setMaximum(self.rt_plot_widget.window_buffer_size)
-        self.window_size_slider.setValue(self.rt_plot_widget.window_size)
-        self.window_size_slider.valueChanged.connect(self.update_window_size)
-        self.window_size_slider_frame_layout.addWidget(self.window_size_slider)
-
-        self.max_label = QLabel(f'{self.rt_plot_widget.window_buffer_duration}')
-        self.max_label.setAlignment(Qt.AlignCenter)
-        self.max_label.setStyleSheet("border: 1px solid gray; background-color: white; border-radius: 4px;")
-        self.window_size_slider_frame_layout.addWidget(self.max_label)
-
-        self.window_size_layout.addWidget(self.window_size_slider_frame)
-
-        # we add this window to the settings one
-        self.settings_layout.addWidget(self.window_size_frame)
-
+        self._generate_complex_slider(title="window offset [n.windows]: ", 
+                                parent=self.frame, 
+                                callback=self.update_window_offset, 
+                                min_shown=f'{0}', 
+                                min = 0,
+                                max_shown=f'{self.rt_plot_widget.window_buffer_factor - 1}', 
+                                max = self.rt_plot_widget.window_buffer_factor - 1,
+                                init_val_shown =f'{self.rt_plot_widget.window_offset}', 
+                                init=self.rt_plot_widget.window_offset)
+        
         # Create a frame for the plot selector
-        self.plot_selector_frame = QFrame(self.settings_scroll_area)
+        self.plot_selector_frame = QFrame(self.frame)
         self.plot_selector_frame.setFrameShape(QFrame.StyledPanel)
         self.plot_selector_layout = QVBoxLayout(self.plot_selector_frame)
-        self.plot_selector_layout.setContentsMargins(5, 5, 5, 5)
+        self.plot_selector_layout.setContentsMargins(2, 2, 2, 2)
 
         # Add title label to the plot selector frame
         plot_selector_title = QLabel("plot selector")
-        self.plot_selector_layout.addWidget(plot_selector_title, alignment=Qt.AlignHCenter)
+        self.plot_selector_layout.addWidget(plot_selector_title, 
+                                alignment=Qt.AlignHCenter)
 
         self.legend_buttons = []
 
@@ -294,26 +331,25 @@ class SettingsWidget(QWidget):
             button.clicked.connect(lambda checked, l=label: self.toggle_line_visibility(l))
             self.legend_buttons.append(button)
             self.plot_selector_layout.addWidget(button)
+        
+        self.settings_frame_layout.addWidget(self.plot_selector_frame)
+        self.settings_frame_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
-        # Add the plot selector frame to the main layout
-        self.settings_layout.addWidget(self.plot_selector_frame)
-
-        # Add another spacer to push the buttons upwards
-        # bottom_spacer = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        # self.settings_scroll_layout.addItem(bottom_spacer)
-
-        # Add a vertical slider for scrolling through legend names
-        self.legend_scroll_slider = QScrollBar(Qt.Vertical, self.plot_selector_frame)
-        self.legend_scroll_slider.valueChanged.connect(self.scroll_legend_names)
-        self.settings_scroll_content.setLayout(self.settings_scroll_layout)
-        self.settings_scroll_area.setWidget(self.settings_scroll_content)
-        self.settings_scroll_area.setVerticalScrollBar(self.legend_scroll_slider)
-
-        self.parent_frame.setLayout(self.settings_layout)
-
-    def update_window_size(self, new_size):
+    def update_window_size(self, 
+                    new_size: int):
 
         self.rt_plot_widget.update_window_size(new_size)
+
+        self.max_labels[1].setText(f'{int(self.rt_plot_widget.window_buffer_duration // self.rt_plot_widget.window_size)}')
+        self.val_sliders[1].setMaximum(int(self.rt_plot_widget.window_buffer_duration // self.rt_plot_widget.window_size))
+        self.current_vals[0].setText(f'{self.rt_plot_widget.update_dt * self.rt_plot_widget.window_size:.2f}')
+
+    def update_window_offset(self, 
+                    offset: int):
+
+        self.rt_plot_widget.update_window_offset(offset)
+
+        self.current_vals[1].setText(f'{self.rt_plot_widget.window_offset}')
 
     def toggle_line_visibility(self, label):
 
@@ -363,45 +399,36 @@ class RtPlotWindow(QWidget):
         self.window_duration = window_duration
 
         self.base_name = base_name
+        
+        # use a QSplitter to handle resizable width between plot and legend frames
+        self.splitter_frame = QFrame()
+        self.splitter_frame.setFrameShape(QFrame.StyledPanel)
 
-        # create the plot frame
-        self.plot_frame = QFrame(main_window.central_widget) # attach to main window
-        self.plot_frame.setFrameShape(QFrame.StyledPanel)
-        self.plot_layout = QVBoxLayout(self.plot_frame)
-        self.plot_layout.setContentsMargins(3, 3, 3, 3)
+        self.splitter = QSplitter(Qt.Horizontal)
 
+        # create the plot widget
         self.rt_plot_widget = RtPlotWidget(
             window_duration=self.window_duration,
             update_dt=self.update_dt,
             n_data=self.n_data,
             base_name=self.base_name,
-            plot_frame=self.plot_frame  # Pass the plot_frame to RtPlotWidget
+            parent=None
         )
-
-        # add the actual plot to the frame
-        self.plot_layout.addWidget(self.rt_plot_widget)
-        self.plot_frame.setLayout(self.plot_layout)
-
-        # create a frame for the legend
-        self.settings_frame = QFrame(main_window.central_widget)
-        self.settings_frame.setFrameShape(QFrame.StyledPanel)
+        # we create the settings widget 
+        self.settings_widget = SettingsWidget(rt_plotter=self.rt_plot_widget, 
+                            parent=None)
         
-        # we add the settings widget 
-        self.settings_window = SettingsWidget(rt_plotter=self.rt_plot_widget, 
-                                        parent_frame=self.settings_frame)
-        
-        # add the plot and legend frames to the main layout
-        main_window.layout.addWidget(self.plot_frame)
-        main_window.layout.addWidget(self.settings_frame, 
-                        alignment=Qt.AlignTop)  # align the legend frame to the top
-
-        # use a QSplitter to handle resizable width between plot and legend frames
-        self.splitter = QSplitter(Qt.Horizontal)
         self.splitter.setHandleWidth(0.1)
-        self.splitter.addWidget(self.plot_frame)
-        self.splitter.addWidget(self.settings_frame)
+        self.splitter.addWidget(self.rt_plot_widget)
+        self.splitter.addWidget(self.settings_widget.frame)
 
-        main_window.layout.addWidget(self.splitter)  # add the splitter to the main layout
+        # Set up the layout
+        self.splitter_layout = QVBoxLayout()
+        self.splitter_layout.addWidget(self.splitter)
+        self.splitter_frame.setLayout(self.splitter_layout)
+        
+        # Set the central widget of the main window
+        self.main_window.setCentralWidget(self.splitter_frame)
 
         # this thread will handle the update of the plot
         self.data_thread = DataThread(self.rt_plot_widget, 
@@ -410,12 +437,35 @@ class RtPlotWindow(QWidget):
                                         Qt.QueuedConnection)
         self.data_thread.start()
         
+class DataThread(QThread):
+
+    data_updated = pyqtSignal(np.ndarray)
+
+    def __init__(self, 
+                rt_plotter: RtPlotWidget, 
+                n_data: int):
+        
+        super().__init__()
+
+        self.rt_plot_widget = rt_plotter
+
+        self.n_data = n_data
+
+    def run(self):
+
+        while True:
+
+            new_data = 2 * (np.random.rand(self.n_data, 1) - np.full((self.n_data, 1), 0.5))
+
+            self.data_updated.emit(new_data)
+
+            time.sleep(self.rt_plot_widget.update_dt)
+
 if __name__ == "__main__":  
 
     app = QApplication(sys.argv)
 
     main_window = RealTimePlotApp()
-    main_window.show()
 
     sys.exit(app.exec_())
 
